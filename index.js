fb56676b59a6a8ed9b7ff154b9a9ffed17f0b92d
Jamie Benjamin <bennythedev@gmail.com>
2022-06-06 17:05:52 -0500

    commit tree 68ee46f8a6164d6cfd50b9e53ba80fe7d1274d1d

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let greeting;
+  if (feet <= 400) {
+    greeting = "This one is on me!"; 
+  } else if (feet > 400 && feet < 2000) {
+    greeting = "That will be twenty bucks.";
+  } else if (feet > 2000 && feet < 2500) {
+    greeting = "I will gladly take your thirty bucks.";
+  } else if (feet > 2500) {
+    greeting = "No can do.";
+  }
+
+  return greeting
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city) {
+  //let city = "NYC"
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  let message;
+    switch (tip) {
+      case 'generous':
+        message = 'Thank you so much.';
+        break;
+      case 'not as generous':
+        message = 'Thank you.';
+        break;
+      default:
+        message = 'Bye.';
+  }
+  return message;
 }
